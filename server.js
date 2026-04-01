7c60b9c657a7003a9ccbebfb48290d4cdb260d9d
situ65 <paridasitu65@gmail.com>
2026-04-02 00:39:31 +0530

    fix express dependency

--- server.js
+++ server.js
@@ -51,7 +51,7 @@ io.on("connection", (socket) => {
 }); // ✅ IMPORTANT CLOSING BRACKET
 
 // server start (OUTSIDE socket block)
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
